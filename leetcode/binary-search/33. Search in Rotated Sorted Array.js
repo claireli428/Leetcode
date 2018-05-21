553fd65751e02807a0f686a80acb631e6005efb3
Claire Li <claireli428@gmail.com>
2018-05-21 17:35:50 -0400

    Update 33. Search in Rotated Sorted Array.js

--- leetcode/binary-search/33. Search in Rotated Sorted Array.js
+++ leetcode/binary-search/33. Search in Rotated Sorted Array.js
@@ -3,8 +3,31 @@
  * @param {number} target
  * @return {number}
  */
+
 var search = function(nums, target) {
-    //first decide middle is above axis or below axis
-    //if middle above axis, then decide if target is between start and middle, it is, keep binary searching, else drop subarray from start to middle
-    //if middle below axis, then decide if target between middle and end, the same as above
+    if(nums.length === 0)
+        return -1;
+
+    var start = 0, end = nums.length - 1;
+    while (start <= end) {
+        var middle = Math.floor((start+end)/2);
+        if(nums[middle] === target)
+            return middle;
+
+        if(nums[middle] > nums[end]) {
+            if(nums[start] <= target && nums[middle] > target)
+                end = middle - 1;
+            else
+                start = middle + 1;
+
+        }
+        else {
+            if(nums[middle] < target && nums[end] >= target)
+                start = middle + 1;
+            else
+                end = middle - 1;
+        }
+    }
+
+    return -1;
 };
