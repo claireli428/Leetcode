e84f232220c063cb0547efc88be221d87cea7964
Claire Li <claireli428@gmail.com>
2018-05-21 17:36:44 -0400

    Update 34. Search for a Range.js

--- leetcode/binary-search/34. Search for a Range.js
+++ leetcode/binary-search/34. Search for a Range.js
@@ -6,13 +6,44 @@
 
 
 var searchRange = function(nums, target) {
+    if(nums.length === 0)
+        return [-1, -1];
+    
+    var first = last = -1, start = 0, end = nums.length - 1;
+    while(start + 1 < end) {
+        var middle = Math.floor((start+end)/2);
+        if (nums[middle] === target) 
+            first = middle;
 
-   //find first target index;
-        //normally, find first target, end while loop
-        //here, since first target might not be first time it appears in the array
-        //so, continue loop, move end point, until start is larger than or equals end
-   // find last target index;
-        //same as fine first index
-   //
-   // return [first, last];
+        if (nums[middle] < target) 
+            start = middle;
+        else 
+            end = middle;
+    }
+
+    if(nums[end] === target)
+        first = end;
+    if(nums[start] === target) 
+        first = start;
+    
+    start = 0, end = nums.length - 1;
+    while(start + 1 < end) {
+        var middle = Math.floor((start+end)/2);
+        if (nums[middle] === target)
+            last = middle;  
+
+        if (nums[middle] > target)
+            end = middle;
+        else
+            start = middle;
+        
+    }
+
+    if(nums[start] === target)
+        last = start;
+    if(nums[end] === target) 
+        last = end;
+
+    return [first, last];
+
 };
