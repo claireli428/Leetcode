898555150d244d9fcd9e5a7b2607606ff5e36765
Claire Li <claireli428@gmail.com>
2018-05-21 17:37:53 -0400

    Update 74. Search a 2D Matrix.js

--- leetcode/binary-search/74. Search a 2D Matrix.js
+++ leetcode/binary-search/74. Search a 2D Matrix.js
@@ -3,9 +3,24 @@
  * @param {number} target
  * @return {boolean}
  */
+
+
 var searchMatrix = function(matrix, target) {
-    //treat it as an sorted mxn array
-    //use binary search
-    //find middle, middle=mxn/2 row index = middle/n, col index = middle  mod m
-}
-;
+    if (!matrix || matrix.length === 0)
+        return false;
+
+    var start = 0, rows = matrix.length, cols = matrix[0].length, end = rows*cols-1;
+    while (start <= end) {
+        var middle = Math.floor((start+end)/2),
+            num = matrix[Math.floor(middle/cols)][middle%cols];
+        if(num === target)
+            return true;
+
+        if (num < target)
+            start = middle + 1;
+        else
+            end = middle - 1;
+    }
+
+    return false;
+};
