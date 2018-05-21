19d1cc89b32bf8b198e0a23857b9d0bffdcb7b3c
Claire Li <claireli428@gmail.com>
2018-05-21 17:37:24 -0400

    Update 35. Search Insert Position.js

--- leetcode/binary-search/35. Search Insert Position.js
+++ leetcode/binary-search/35. Search Insert Position.js
@@ -3,9 +3,27 @@
  * @param {number} target
  * @return {number}
  */
+
 var searchInsert = function(nums, target) {
-    //find by binary search
-    //if exist, return middle
-    //else it could only be before, middle, after start and end
+    if(nums.length === 0)
+        return 0;
+
+    var start = 0, end = nums.length - 1;
+    while (start + 1 < end) {
+        var middle = Math.floor((start+end)/2);
+        if(nums[middle] === target)
+            return middle;
+        if(nums[middle] < target)
+            start = middle;
+        else
+            end = middle;
+    }
+
+    if(nums[start] >= target)
+        return start;
+    if(nums[end] < target)
+        return end + 1;
+
+    return end;
 
 };
