eec02c63c80201c56981c50bae645d7926099dbe
Claire Li <claireli428@gmail.com>
2018-05-21 17:51:31 -0400

    Update 240.Search a 2D Matrix.js

--- leetcode/binary-search/240.Search a 2D Matrix.js
+++ leetcode/binary-search/240.Search a 2D Matrix.js
@@ -2,9 +2,23 @@
  * @param {number[][]} matrix
  * @param {number} target
  * @return {boolean}
- *
- * 2 binary search or find right down to left
  */
 var searchMatrix = function(matrix, target) {
+    if(matrix.length === 0 || matrix[0].length === 0)
+        return false;
+    
+    var r = 0, c = matrix[0].length - 1;
 
+    while (r < matrix.length && c >= 0) {
+        if(matrix[r][c] === target)
+            return true;
+        
+        if(matrix[r][c] < target)
+            r++;
+        else
+            c--;
+    }
+
+    return false;
+    
 };
