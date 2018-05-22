620c240224adbae477168b6932a36460042f0ef4
Claire Li <claireli428@gmail.com>
2018-05-22 17:41:55 -0400

    Update 153. Find Minimum in Rotated Sorted Array.js

--- leetcode/binary-search/153. Find Minimum in Rotated Sorted Array.js
+++ leetcode/binary-search/153. Find Minimum in Rotated Sorted Array.js
@@ -1,10 +1,17 @@
 /**
  * @param {number[]} nums
  * @return {number}
- *
- * if medium is above axis, drop start-medium
- * if medium is below axis, drop medium-end
  */
 var findMin = function(nums) {
+    
+    var start = 0, end = nums.length - 1;
+    while (start + 1 < end) {
+        var middle = Math.floor((start+end)/2);
+        if (nums[middle] > nums[end])
+            start = middle;
+        else
+            end = middle;
+    } 
 
+    return Math.min(nums[start], nums[end]);
 };
