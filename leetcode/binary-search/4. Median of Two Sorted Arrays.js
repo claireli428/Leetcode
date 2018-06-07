31bede81d69b9d81cf09d6636a80fc8e3d746c7a
Claire Li <claireli428@gmail.com>
2018-06-07 17:57:50 -0400

    Update 4. Median of Two Sorted Arrays.js

--- leetcode/binary-search/4. Median of Two Sorted Arrays.js
+++ leetcode/binary-search/4. Median of Two Sorted Arrays.js
@@ -2,16 +2,32 @@
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
- *
- * Method 1, find i(k/2) number from A, find j(k-i) number from B
- * if [i-i] < [j] && [j-1] < [i] return medium
- * else if [i-i] > j, drop i-end from A
- * else [j-1] > [i], drop j-end from B
- *
- * Method 2, findkth(nums1, nums2, k)
  * take k/2 from nums1 and nums 2, if nums1[k/2] < nums2[k/2], drop 0-k/2 from nums1(new nums1)
  * findkth(new nums1, nums2, k-k/2)
  */
-var findMedianSortedArrays = function(nums1, nums2) {
+var findKth = function(A, B, k) {
+    if(A.length === 0)
+        return B[k-1];
+    if(B.length === 0)
+        return A[k-1];
+    if(k <= 1)
+        return Math.min(A[0], B[0]);
 
+
+    var l = Math.min(parseInt(k/2), A.length, B.length), newA = A, newB = B;
+    if(A[l-1] < B[l-1])
+        newA = A.length > l ? A.slice(l) : [];
+    else
+        newB = B.length > l ? B.slice(l) : [];
+
+    return findKth(newA, newB, k-l);
+
+};
+
+var findMedianSortedArrays = function(nums1, nums2) {
+    var n1 = nums1.length, n2 = nums2.length;
+    if((n1+n2)%2 === 0)
+        return (findKth(nums1, nums2, (n1+n2)/2) + findKth(nums1, nums2, (n1+n2)/2 + 1))/2;
+    else
+        return findKth(nums1, nums2, (n1+n2+1)/2);
 };
