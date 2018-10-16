a4f56d1509a586a84bf3cd5d2480ace1f5b6f7c9
Claire Li <claireli428@gmail.com>
2018-10-16 10:56:53 -0400

    Update 606ConstructStringfromBinaryTree.js

--- leetcode/binary-tree/606ConstructStringfromBinaryTree.js
+++ leetcode/binary-tree/606ConstructStringfromBinaryTree.js
@@ -14,7 +14,7 @@ var tree2str = function (t) {
     return t.val + '(' + tree2str(t.left) + ')(' + tree2str(t.right) + ')';
 };
 
-//DFS
+//DFS 1
 var tree2str = function (t) {
     if (!t) return '';
 
@@ -41,6 +41,38 @@ var tree2str = function (t) {
             stack.push(top.left);
         } else {
             res += '()';
+        }
+    }
+
+    return res.slice(1, res.length - 1);
+};
+
+//DFS 2
+var tree2str = function (t) {
+    if (!t) return '';
+
+    let res = '', stack = [t], visited = new Set();
+    while (stack.length) {
+        let top = stack[stack.length - 1];
+        if(visited.has(top)) {
+            res += ')';
+            stack.pop();
+            continue;
+        }
+
+        res += '(' + top.val;
+        visited.add(top);
+        if(!top.left && !top.right) {
+            res += ')';
+            stack.pop();
+        } else if (!top.left) {
+            res += '()';
+            stack.push(top.right);
+        } else if (!top.right) {
+            stack.push(top.left);
+        } else {
+            stack.push(top.right);
+            stack.push(top.left);
         }
     }
 
