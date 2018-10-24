d893a470c6d72a85598ee4e445973530c4dbadf7
Claire Li <claireli428@gmail.com>
2018-10-24 11:49:39 -0400

    Update 671SecondMinumumNodeInaBinaryTree.js

--- leetcode/binary-tree/671SecondMinumumNodeInaBinaryTree.js
+++ leetcode/binary-tree/671SecondMinumumNodeInaBinaryTree.js
@@ -19,3 +19,24 @@ var findSecondMinimumValue = function(root) {
     if(right === -1) return left;
     return Math.min(left, right);
 };
+
+//DFS
+var findSecondMinimumValue = function(root) {
+    if(!root || !root.left) return -1;
+
+    let stack = [root], secondMin = Math.max(root.left.val, root.right.val);
+    while(stack.length) {
+        const top = stack.pop();
+
+        if(!top.left && top.val > root.val && top.val < secondMin) secondMin = top.val; 
+
+        if(top.left) {
+            const max = Math.max(top.left.val, top.right.val)
+            if(max > root.val && max < secondMin) secondMin = max;
+            stack.push(top.right);
+            stack.push(top.left);
+        }
+    }
+
+    return secondMin === root.val ? -1 : secondMin;
+};
