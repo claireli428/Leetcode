e9372eebc09f3a295253caaeb98355f673991509
Claire Li <claireli428@gmail.com>
2018-10-30 14:20:59 -0400

    Create 46Permutations.js

---
+++ leetcode/back-tracking/46Permutations.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    let res = [];
+    let visited = [];
+    permuteHelper(nums, res, visited);
+    return res;
+};
+
+var permuteHelper = function(remains, res, visited) {
+    if(!remains.length) {
+        res.push(visited);
+        return;
+    }
+
+    for(let j = 0; j < remains.length; j++) {
+        if(!visited.includes(remains[j])) {
+            const newVisited = [...visited, remains[j]];
+            const newRemains = [...remains.slice(0, j), ...remains.slice(j+1)];
+            permuteHelper(newRemains, res, newVisited);
+        }
+    }
+
+}
