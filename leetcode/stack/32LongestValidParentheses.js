692cab9509c02c4ff13494d57ba1ec50037dcf00
Claire Li <claireli428@gmail.com>
2018-11-02 17:49:07 -0400

    Update 32LongestValidParentheses.js

--- leetcode/stack/32LongestValidParentheses.js
+++ leetcode/stack/32LongestValidParentheses.js
@@ -2,7 +2,24 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function(s) {
+var longestValidParentheses = function (s) {
+    if (!s.length) return 0;
 
+    let stack = [], start = -1, res = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        } else if (!stack.length) {
+            start = i;
+        } else {
+            stack.pop();
+            if (!stack.length) {
+                res = Math.max(res, i - start);
+            } else {
+                res = Math.max(res, i - stack[stack.length - 1]);
+            }
+        }
+    }
 
+    return res;
 };
