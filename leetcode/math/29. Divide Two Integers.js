f56b249cc4fab5f9b28bcc1f5348b03969d98875
Claire Li <claireli428@gmail.com>
2018-11-13 11:11:18 -0500

    Update 29. Divide Two Integers.js

--- leetcode/math/29. Divide Two Integers.js
+++ leetcode/math/29. Divide Two Integers.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var divide = function (dividend, divisor) {
-    if (dividend === -Math.pow(2, 31) && divisor === -1) return Math.pow(2, 31) - 1;
-    if (Math.abs(divisor) === 1) return dividend*divisor;
+    if (dividend === -2147483648 && divisor === -1) return 2147483647;
+    if (Math.abs(divisor) === 1) return dividend * divisor;
 
-    const sign = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0) ? -1 : 1;
+    const sign = dividend * divisor < 0 ? -1 : 1;
     dividend = Math.abs(dividend), divisor = Math.abs(divisor);
     let times = 0;
     while (dividend >= divisor) {
         let diff = 1, temp = divisor;
-        while (dividend >= (temp << 1)) {
+        const num = dividend >> 1;
+        while (num >= temp) {
             temp <<= 1;
             diff <<= 1;
         }
