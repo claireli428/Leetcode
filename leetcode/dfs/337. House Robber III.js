8a75dabec0131560ff3affd939dfad4942126dd7
Claire Li <claireli428@gmail.com>
2018-11-26 17:00:19 -0500

    Update 337. House Robber III.js

--- leetcode/dfs/337. House Robber III.js
+++ leetcode/dfs/337. House Robber III.js
@@ -9,6 +9,8 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+//Solution 1
 var rob = function (root) {
   let robbed = new Map();
   return helper(root, robbed);
@@ -28,4 +30,21 @@ var helper = function (root, robbed) {
   const res = Math.max(robRoot, notRobRoot);
   robbed.set(root, res)
   return res;
+}
+
+//Solution 2
+var rob = function (root) {
+  let res = dfs(root);
+  return Math.max(res[0], res[1]);
+};
+
+var dfs = function (root) {
+  if (!root) return [0, 0];
+
+  const left = dfs(root.left), right = dfs(root.right);
+
+  const robRoot = root.val + left[1] + right[1];
+  const notRobRoot = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+  return [robRoot, notRobRoot];
 }
