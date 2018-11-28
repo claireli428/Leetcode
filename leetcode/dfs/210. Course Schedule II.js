e53714214304e70a09834906370725c8aaf89aa6
Claire Li <claireli428@gmail.com>
2018-11-28 18:38:24 -0500

    Update 210. Course Schedule II.js

--- leetcode/dfs/210. Course Schedule II.js
+++ leetcode/dfs/210. Course Schedule II.js
@@ -5,30 +5,71 @@
  */
 
 //BFS
+var findOrder = function (numCourses, prerequisites) {
+  const { graph, starts, indegreeDict } = constructBFSGraph(numCourses, prerequisites);
+  if (!starts.size) return [];
 
+  let res = [], queue = [];
+  for(let start of starts.values()) queue.push(start);
+
+  while(queue.length) {
+    const start = queue.pop();
+    res.push(start);
+    const outdegrees = graph.get(start);
+    if(outdegrees) {
+      for(let vertex of outdegrees.values()) {
+        indegreeDict[vertex] -= 1;
+        if(indegreeDict[vertex] === 0) queue.push(vertex);
+      }
+    }
+  }
+
+  return res.length === numCourses ? res : [];
+};
+
+var constructBFSGraph = function (numCourses, prerequisites) {
+  let graph = new Map(), starts = new Set(), indegreeDict = new Array(numCourses).fill(0);
+  for (let i = 0; i < numCourses; i++) {
+    starts.add(i);
+  }
+
+  for (let i = 0; i < prerequisites.length; i++) {
+    const idx = prerequisites[i][0]; 
+    indegreeDict[idx] += 1;
+
+    if (starts.has(prerequisites[i][0])) starts.delete(prerequisites[i][0]);
+
+    const vertex = prerequisites[i][1];
+    let outdegrees = graph.has(vertex) ? graph.get(vertex) : new Set();
+    outdegrees.add(prerequisites[i][0]);
+    graph.set(vertex, outdegrees);
+  }
+
+  return { graph: graph, starts: starts, indegreeDict: indegreeDict };
+};
 
 //DFS
-var findOrder = function(numCourses, prerequisites) {
-  const { graph, starts } = constructGraph(numCourses, prerequisites);
-  if(!starts.size) return [];
+var findOrder = function (numCourses, prerequisites) {
+  const { graph, starts } = constructDFSGraph(numCourses, prerequisites);
+  if (!starts.size) return [];
 
   let res = [], visited = new Set();
-  for(let start of starts.values()) {
-    if(!canFinishHelper(res, visited, graph, start)) return [];
+  for (let start of starts.values()) {
+    if (!canFinishHelper(res, visited, graph, start)) return [];
   }
 
   return res.length === numCourses ? res : [];
 };
 
 var canFinishHelper = function (res, visited, graph, start) {
-  if(visited.has(start) && !res.includes(start)) return false;
+  if (visited.has(start) && !res.includes(start)) return false;
 
   visited.add(start);
   let outdegrees = graph.get(start);
-  if(outdegrees) {
-    for(let outdegree of outdegrees.values()) {
-      if(!res.includes(outdegree)) {
-        if(!canFinishHelper(res, visited, graph, outdegree)) return false;
+  if (outdegrees) {
+    for (let vertex of outdegrees.values()) {
+      if (!res.includes(vertex)) {
+        if (!canFinishHelper(res, visited, graph, vertex)) return false;
       }
     }
   }
@@ -37,7 +78,7 @@ var canFinishHelper = function (res, visited, graph, start) {
   return true;
 };
 
-var constructGraph = function (numCourses, prerequisites) {
+var constructDFSGraph = function (numCourses, prerequisites) {
   let graph = new Map(), starts = new Set();
   for (let i = 0; i < numCourses; i++) {
     starts.add(i);
@@ -46,10 +87,10 @@ var constructGraph = function (numCourses, prerequisites) {
   for (let i = 0; i < prerequisites.length; i++) {
     if (starts.has(prerequisites[i][1])) starts.delete(prerequisites[i][1]);
 
-    const key = prerequisites[i][0];
-    let values = graph.has(key) ? graph.get(key) : new Set();
-    values.add(prerequisites[i][1]);
-    graph.set(key, values);
+    const vertex = prerequisites[i][0];
+    let outdegrees = graph.has(vertex) ? graph.get(vertex) : new Set();
+    outdegrees.add(prerequisites[i][1]);
+    graph.set(vertex, outdegrees);
   }
 
   return { graph: graph, starts: starts };
