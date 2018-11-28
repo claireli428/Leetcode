b921447b282192ee97521a36c2ed090df2cf1074
Claire Li <claireli428@gmail.com>
2018-11-28 14:49:25 -0500

    Create 207. Course Schedule.js

---
+++ leetcode/dfs/207. Course Schedule.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+
+var canFinish = function (numCourses, prerequisites) {
+  let { graph, starts } = constructGraph(numCourses, prerequisites);
+  if (!starts.size) return false;
+
+  let visited = new Set(), memo = new Set();
+  for (let start of starts.values()) {
+    if (!exploreCycle(memo, visited, graph, start)) return false;
+  }
+
+  return visited.size === numCourses;
+};
+
+var exploreCycle = function (memo, visited, graph, start) {
+  if (memo.has(start)) return true;
+
+  if (visited.has(start)) return false;
+
+  visited.add(start);
+  let indegrees = graph.get(start);
+  if (indegrees) {
+    for (let indegree of indegrees.values()) {
+      if (!exploreCycle(memo, visited, graph, indegree)) return false;
+    }
+  }
+
+  memo.add(start);
+  return true;
+};
+
+var constructGraph = function (numCourses, prerequisites) {
+  let graph = new Map(), starts = new Set();
+  for (let i = 0; i < numCourses; i++) {
+    starts.add(i);
+  }
+
+  for (let i = 0; i < prerequisites.length; i++) {
+    if (starts.has(prerequisites[i][1])) starts.delete(prerequisites[i][1]);
+
+    const key = prerequisites[i][0];
+    let values = graph.has(key) ? graph.get(key) : new Set();
+    values.add(prerequisites[i][1]);
+    graph.set(key, values);
+  }
+
+  return { graph: graph, starts: starts };
+};
