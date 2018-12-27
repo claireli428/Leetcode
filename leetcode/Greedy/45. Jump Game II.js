dbc46ff6abf9d92b6f2844e2c10ef9f5417cdf19
Claire Li <claireli428@gmail.com>
2018-12-27 12:02:02 -0500

    Update 45. Jump Game II.js

--- leetcode/Greedy/45. Jump Game II.js
+++ leetcode/Greedy/45. Jump Game II.js
@@ -2,10 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
+//Greedy
+var jump = function (nums) {
+  if (nums.length < 2) return 0;
+
+  let curJump = 1;
+  let curEnd = nums[0];
+  let farthest = nums[0];
+  for (let i = 1; i < nums.length - 1; i++) {
+    farthest = Math.max(farthest, i + nums[i]);
+    if (i === curEnd) {
+      curJump++;
+      curEnd = farthest;
+    }
+  }
+
+  return curJump;
+};
 
 //DP 
 var jump = function (nums) {
-  if (!nums.length) return 0;
+  if (nums.length < 2) return 0;
 
   let n = nums.length;
   let dp = new Array(n).fill(n);
