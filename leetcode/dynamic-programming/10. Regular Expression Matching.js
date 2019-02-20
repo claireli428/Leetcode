ed0c7a7fff0ec1410e64467052cacf8eaa570d61
Claire Li <claireli428@gmail.com>
2019-02-20 13:10:03 -0500

    Update 10. Regular Expression Matching.js

--- leetcode/dynamic-programming/10. Regular Expression Matching.js
+++ leetcode/dynamic-programming/10. Regular Expression Matching.js
@@ -2,36 +2,29 @@
  * @param {string} s
  * @param {string} p
  * @return {boolean}
+ * 
+ * dp[i][j]: is first i characters in s matching first j characters in p
  */
-var isMatch = function(s, p) {
-    let dp = new Array(s.length+1).fill(null).map(() => new Array(p.length+1).fill(false));
-    dp[0][0] = true;
-    for (let i = 1; i <= s.length; i++) {
-        dp[i][0] = false;
-    }
-    for (let j = 1; j <= p.length; j++) {
-        if(p[j-1] === '*') {
-            dp[0][j] = dp[0][j-2];
-        } else {
-            dp[0][j] = false;
-        }
-    }
+var isMatch = function (s, p) {
+  let dp = new Array(s.length + 1).fill(false).map(() => new Array(p.length + 1).fill(false));
 
-    for(let i = 1; i <= s.length; i++) {
-        for (let j = 1; j <=p.length; j++) {
-            let sIdx = i - 1, pIdx = j - 1;
-            if(s[sIdx] === p[pIdx] || p[pIdx] === '.') {
-                dp[i][j] = dp[i-1][j-1];
-            } else if(p[pIdx] === '*') {
-                if(p[pIdx-1] === s[sIdx] || p[pIdx-1] === '.')
-                    dp[i][j] = dp[i][j-2] || dp[i-1][j-1] || dp[i-1][j];
-                else {
-                    dp[i][j] = dp[i][j-2];
-                }
-            }
-        }
+  dp[0][0] = true;
+  for (let j = 2; j <= p.length; j++) {
+    dp[0][j] = p[j - 1] === '*' ? dp[0][j - 1] || dp[0][j - 2] : false;
+  }
+
+  if (s.length > 0) dp[1][1] = s[0] === p[0] || p[0] === '.';
+
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 2; j <= p.length; j++) {
+      if (s[i - 1] === p[j - 1] || p[j - 1] === '.') {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (p[j - 1] === '*') {
+        //'*' match one preceding character or zero preceding character or multiple preceding characters
+        dp[i][j] = dp[i][j - 1] || dp[i][j - 2] || (dp[i - 1][j] && (p[j - 2] === s[i - 1] || p[j - 2] === '.'));
+      }
     }
+  }
 
-
-    return dp[s.length][p.length];
+  return dp[s.length][p.length];
 };
