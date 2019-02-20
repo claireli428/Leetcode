70d8f4f0c018b4e76f94b5b199b41e9391c527db
Claire Li <claireli428@gmail.com>
2019-02-20 12:46:41 -0500

    Update 44. Wildcard Matching.js

--- leetcode/dynamic-programming/44. Wildcard Matching.js
+++ leetcode/dynamic-programming/44. Wildcard Matching.js
@@ -2,31 +2,27 @@
  * @param {string} s
  * @param {string} p
  * @return {boolean}
+ * 
+ * dp[i][j]: is first i characters in s matching first j characters in p
  */
-var isMatch = function(s, p) {
-    let dp = new Array(s.length+1).fill(null).map(() => new Array(p.length+1).fill(false));
-    dp[0][0] = true;
-    for (let i = 1; i <= s.length; i++) {
-        dp[i][0] = false;
-    }
+var isMatch = function (s, p) {
+  let dp = new Array(s.length + 1).fill(false).map(() => new Array(p.length + 1).fill(false));
+
+  dp[0][0] = true;
+  for (let j = 1; j <= p.length; j++) {
+    dp[0][j] = p[j - 1] === '*' ? dp[0][j - 1] : false;
+  }
+
+  for (let i = 1; i <= s.length; i++) {
     for (let j = 1; j <= p.length; j++) {
-        if(p[j-1] === '*') {
-            dp[0][j] = dp[0][j-1];
-        } else {
-            dp[0][j] = false;
-        }
+      if (s[i - 1] === p[j - 1] || p[j - 1] === '?') {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (p[j - 1] === '*') {
+        //'*' match zero character or match multiple characters
+        dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+      }
     }
+  }
 
-    for(let i = 1; i <= s.length; i++) {
-        for (let j = 1; j <=p.length; j++) {
-            let sIdx = i - 1, pIdx = j - 1;
-            if(s[sIdx] === p[pIdx] || p[pIdx] === '?') {
-                dp[i][j] = dp[i-1][j-1];
-            } else if(p[pIdx] === '*') {
-                dp[i][j] = dp[i][j-1] || dp[i-1][j-1] || dp[i-1][j];
-            }
-        }
-    }
-
-    return dp[s.length][p.length];
+  return dp[s.length][p.length];
 };
