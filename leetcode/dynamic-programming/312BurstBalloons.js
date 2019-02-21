2ece89b16e912292bdc74b54c875ddb39b5a8d9e
Claire Li <claireli428@gmail.com>
2019-02-21 16:55:17 -0500

    Update 312BurstBalloons.js

--- leetcode/dynamic-programming/312BurstBalloons.js
+++ leetcode/dynamic-programming/312BurstBalloons.js
@@ -1,23 +1,24 @@
 /**
  * @param {number[]} nums
  * @return {number}
+ * 
+ * dp[i][j]: max coins yeilded by bursting ballons between i to j
+ * i----------------k--------------j
+ * dp[i][j] = max(dp[i][k] + nums[k] * nums[i] * nums[j] + dp[k+1][j], k = i+1 ... j-1)
  */
-var maxCoins = function(nums) {
-    if(!nums.length) return 0;
+var maxCoins = function (nums) {
+  let n = nums.length;
+  let extendedNums = [1, ...nums, 1];
+  let dp = new Array(n + 2).fill(0).map(() => new Array(n + 2).fill(0));
 
-    nums.unshift(1);
-    nums.push(1);
-    const n = nums.length;
-    let dp = new Array(n).fill(null).map(() => new Array(n).fill(0));
-    
-    for(let len = 1; len <= n - 2; len++) {
-        for(let i = 0; i <= n - len -2; i++) {
-            let j = i + len + 1;
-            for(let k = i+1; k < j; k++) {
-                dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + nums[i]*nums[k]*nums[j]);
-            }
-        }
+  for (let len = 1; len <= n; len++) {
+    for (let i = 0; i < n + 2 - len; i++) {
+      let j = i + len + 1;
+      for (let k = i + 1; k < j; k++) {
+        dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + extendedNums[i] * extendedNums[k] * extendedNums[j]);
+      }
     }
+  }
 
-    return dp[0][n-1];
+  return dp[0][n + 1];
 };
