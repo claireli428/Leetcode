0f3aeb6e42b82f1a720639dbf27a350fb568c063
Claire Li <claireli428@gmail.com>
2019-08-09 16:10:26 -0400

    Update 84. Largest Rectangle in Histogram.js

--- leetcode/stack/84. Largest Rectangle in Histogram.js
+++ leetcode/stack/84. Largest Rectangle in Histogram.js
@@ -1,35 +1,55 @@
 /**
  * @param {number[]} heights
  * @return {number}
+ * 
+ * For each height, the max area is:
+ * (height) * (distance between first previous less and first next less)
+ * 
+ * Brute Force
+ * Two loops:
+ * Outer loop (each height)
+ * Inner loop (from itself, search previous and next directions to find first less)
+ * Calc each max area in outer loop and take maximum
+ * Time Complexity: O(n*n)
+ * Space Complexity: O(1)
+ * 
+ * Optimization: there're repeating steps for find previous and next less for every height
+ * Key point: Use monotonous stack to find previous/next less of all heights, time commplexity O(n)
+ * Solution: 
+ * 1. One direction at a time, find preLess/nextLess for each height
+ * 2. Calc max area for each height and take maximum
+ * Time Complexity: O(n) + O(n) + O(n) = O(n)
+ * Space Complexity: O(n)
  */
-var largestRectangleArea = function(heights) {
-    if(!heights.length) return 0;
 
-    let preLess = new Map(), nextLess = new Map();
-    let stack = [];
-    for(let i = 0; i < heights.length; i++) {
-      while(stack.length && heights[i] < heights[stack[stack.length - 1]]) {
-        let top = stack.pop();
-        nextLess.set(top, i);
-      }
-      stack.push(i);
+var largestRectangleArea = function (heights) {
+  let stack = [];
+  let nextLess = new Array(heights.length).fill(heights.length);
+  for (let i = 0; i < heights.length; i++) {
+    while (stack.length && heights[stack[stack.length - 1]] > heights[i]) {
+      let idx = stack.pop();
+      nextLess[idx] = i;
     }
 
-    stack = [];
-    for(let i = heights.length - 1; i >= 0; i--) {
-      while(stack.length && heights[i] < heights[stack[stack.length - 1]]) {
-        let top = stack.pop();
-        preLess.set(top, i);
-      }
-      stack.push(i);
+    stack.push(i);
+  }
+
+  stack = [];
+  let preLess = new Array(heights.length).fill(-1);
+  for (let i = heights.length - 1; i >= 0; i--) {
+    while (stack.length && heights[stack[stack.length - 1]] > heights[i]) {
+      let idx = stack.pop();
+      preLess[idx] = i;
     }
 
-    let res = 0;
-    for(let i = 0; i < heights.length; i++) {
-      let left = preLess.has(i) ? preLess.get(i) : -1;
-      let right = nextLess.has(i) ? nextLess.get(i) : heights.length;
-      res = Math.max(res, (right - left - 1) * heights[i]);
-    }
+    stack.push(i);
+  }
 
-    return res;
+  let res = 0;
+  for (let i = 0; i < heights.length; i++) {
+    res = Math.max(res, heights[i] * (nextLess[i] - preLess[i] - 1));
+  }
+
+  return res;
+
 };
