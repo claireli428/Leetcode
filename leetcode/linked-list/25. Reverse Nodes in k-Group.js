a1c08b0308d2378025cb1672d4ae6f8037f4bf32
Claire Li <claireli428@gmail.com>
2019-08-12 22:15:57 -0400

    Update 25. Reverse Nodes in k-Group.js

--- leetcode/linked-list/25. Reverse Nodes in k-Group.js
+++ leetcode/linked-list/25. Reverse Nodes in k-Group.js
@@ -10,47 +10,34 @@
  * @param {number} k
  * @return {ListNode}
  */
-var reverseKGroup = function (head, k) {
-  if (!head || k === 1) return head;
+var reverseKGroup = function(head, k) {
+    if(!head) return head;
+    
+    let dummy = new ListNode();
+    dummy.next = head;
+    let pre = dummy;
+    let cur = head;
+    
+    while (cur) {
+        let count = 0;
+        let ptr = cur;
+        while (ptr && count < k) {
+            count++;
+            ptr = ptr.next;
+        }
+        
+        if(count < k) break;
+        
+        for(let i = 1; i < k; i++) {
+            let temp = cur.next;
+            cur.next = temp.next;
+            temp.next = pre.next;
+            pre.next = temp;
+        }
+        pre = cur;
+        cur = cur.next;
+    } 
+    
+    return dummy.next;
 
-  let Dummy = new ListNode(0);
-  Dummy.next = head;
-  let dummy = Dummy;
-  let tail = dummy.next;
-
-  while (dummy.next) {
-    //count k nodes
-    let count = 0, ptr = dummy;
-    while (ptr && ptr.next && count < k) {
-      ptr = ptr.next;
-      count++;
-    }
-
-    if (count < k) break;
-
-    //remove tail from group
-    dummy.next = dummy.next.next;
-
-    //link rest list to tail
-    tail.next = ptr.next;
-    ptr.next = null;
-
-    //reverse group
-    let cur = dummy.next;
-    while (cur && cur.next) {
-      let temp = cur.next;
-      cur.next = cur.next.next;
-      temp.next = dummy.next;
-      dummy.next = temp;
-    }
-
-    //link group to tail
-    cur.next = tail;
-
-    //update dummy & tail
-    dummy = tail;
-    tail = dummy.next;
-  }
-
-  return Dummy.next;
 };
