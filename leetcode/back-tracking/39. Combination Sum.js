e073c1d2b87cfe994629f79718b2720e529652d1
Claire Li <claireli428@gmail.com>
2019-09-04 23:20:06 -0400

    Update 39. Combination Sum.js

--- leetcode/back-tracking/39. Combination Sum.js
+++ leetcode/back-tracking/39. Combination Sum.js
@@ -27,3 +27,38 @@ var helper = function(candidates, target, sum, start, res, current) {
   }
 
 };
+
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+    let res = [];
+    combinationSumDFS(candidates, res, target, [], 0);
+    console.log(res);
+    const finalRes = res.map(times => {
+        let combination = [];
+        times.forEach((e, i) => {
+            combination = [...combination, ...new Array(e).fill(candidates[i])];
+        });
+        return combination;
+    });
+    return finalRes;
+};
+
+var combinationSumDFS = function (candidates, res, target, path, level) {
+    if(target === 0) {
+        res.push(path.slice(0));
+        return;
+    }
+    
+    if(level === candidates.length) return;
+
+    for(let i = 0; i * candidates[level] <= target; i++) {
+        path.push(i);
+        combinationSumDFS(candidates, res, target - i * candidates[level], path, level + 1);
+        path.pop();
+    }
+};
